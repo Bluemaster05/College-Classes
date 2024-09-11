871ca263906b10918cd55b16ebf460fc9983bb29
Bluemaster05 <logang@eatonair.us>
2024-09-11 18:44:14 -0400

    fixed zybug

--- 215-CPTR/Class/9-5-24/test.py
+++ 215-CPTR/Class/9-5-24/test.py
@@ -2,7 +2,7 @@ employees = input().split()
 hours = input().split()
 separator_char = input()
 
-print(f"{"Employees":^26}|{"Hours":>26}")
+print(f"{'Employees':^26}|{'Hours':>26}")
 print(f"{separator_char * 52}")
 print(f"{employees[0]:>26}|{hours[0]:>26}")
 print(f"{employees[1]:>26}|{hours[1]:>26}")
