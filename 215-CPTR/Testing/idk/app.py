e539cbe8c74abebf93a336ce61dfe3be8be6d2c9
Bluemaster05 <logang@eatonair.us>
2024-09-24 06:04:28 -0400

    testing.py

--- 215-CPTR/Testing/idk/app.py
+++ 215-CPTR/Testing/idk/app.py
@@ -1,10 +1,24 @@
-words = input().split()
+# The words parameter is a list of strings.
+def build_dictionary(words):
+    # The frequencies dictionary will be built with your code below.
+    # Each key is a word string and the corresponding value is an integer 
+    # indicating that word's frequency.
+    dictionary = {}
+    occurances = 0
+    for word in words:
+        if word not in dictionary:
+            for item in words:
+                if word == item:
+                    occurances += 1
+    dictionary[word] = occurances
+    occurances = 0
+    return dictionary
 
-
-counter = 0
-for i in range(len(words)):
-    for j in words:
-        if words[i].lower() == j.lower():
-            counter += 1
-    print(f"{j} {counter}")
-    counter = 0
+# The following code asks for input, splits the input into a word list, 
+# calls build_dictionary(), and displays the contents sorted by key.
+if __name__ == '__main__':
+    words = input().split()
+    your_dictionary = build_dictionary(words)
+    sorted_keys = sorted(your_dictionary.keys())
+    for key in sorted_keys:
+        print(f'{ key } - { str(your_dictionary[key]) }')
