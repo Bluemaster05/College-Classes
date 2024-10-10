37143e9820782ac00e7e50b0d5fe8d66107d2fd4
Bluemaster05 <logang@eatonair.us>
2024-10-10 09:10:56 -0400

    uno tests

--- 215-CPTR/Lab/10-1-24/Lab/uno.py
+++ 215-CPTR/Lab/10-1-24/Lab/uno.py
@@ -8,12 +8,72 @@ class UnoCard:
         self.rank = rank
 
     def can_be_played_on(self, other: "UnoCard"):
+        """Checks if card other can be played on card self. Returns Boolean Expression
+        >>> card1 = UnoCard("K", "W")
+        >>> card2 = UnoCard("K", "F")
+        >>> card3 = UnoCard("Y", "5")
+        >>> card4 = UnoCard("B", "S")
+        >>> card5 = UnoCard("B", "5")
+        >>> card1.can_be_played_on(card2)
+        True
+        >>> card1.can_be_played_on(card3)
+        True
+        >>> card4.can_be_played_on(card3)
+        False
+        >>> card5.can_be_played_on(card3)
+        True
+        
+        """
         if self.color == "K" or other.color == "K" or self.color == other.color or self.rank == other.rank:
             return True
         else:
             return False
 
     def score_value(self):
+        """ Returns the score value of the card
+        >>> card1 = UnoCard("K", "W")
+        >>> card2 = UnoCard("K", "F")
+        >>> card3 = UnoCard("Y", "0")
+        >>> card4 = UnoCard("B", "1")
+        >>> card5 = UnoCard("B", "2")
+        >>> card6 = UnoCard("B", "3")
+        >>> card7 = UnoCard("B", "4")
+        >>> card8 = UnoCard("B", "5")
+        >>> card9 = UnoCard("B", "6")
+        >>> card10 = UnoCard("B", "7")
+        >>> card11 = UnoCard("B", "8")
+        >>> card12 = UnoCard("B", "9")
+        >>> card13 = UnoCard("B", "S")
+        >>> card14 = UnoCard("B", "D") 
+        >>> card1.score_value()
+        50
+        >>> card2.score_value()
+        50
+        >>> card3.score_value()
+        0
+        >>> card4.score_value()
+        1
+        >>> card5.score_value()
+        2
+        >>> card6.score_value()
+        3
+        >>> card7.score_value()
+        4
+        >>> card8.score_value()
+        5
+        >>> card9.score_value()
+        6
+        >>> card10.score_value()
+        7
+        >>> card11.score_value()
+        8
+        >>> card12.score_value()
+        9
+        >>> card13.score_value()
+        20
+        >>> card14.score_value()
+        20
+        """
         scoreRanks = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
         twentyRanks = ["S", "D", "R"]
         fiftyRanks = ["W", "F"]
@@ -25,12 +85,18 @@ class UnoCard:
             return 50
 
     def __str__(self) -> str:
+        """Returns a human-readable string representation of the card
+        """
         return f"{self.color}{self.rank}"
 
     def __repr__(self) -> str:
+        """Returns a python-readable string representation that can be used to recreate the card
+        """
         return f"UnoCard('{self.color}', '{self.rank}')"
 
 def create_deck():
+    """Create a Full Uno deck and shuffles it
+    """
     standardRanks = "1 2 3 4 5 6 7 8 9 S D R".split(" ")
     standardColors = "R G B Y".split(" ")
     blackRanks = ["W", "F"]
@@ -49,6 +115,8 @@ def create_deck():
  
 
 def deal_hands(deck, numhands):
+    """Takes a deck and distributes 7 cards for each hand into a list of lists, the cards are distibuted so that they.
+    """
     decks = []
     for i in range(numhands):
         decks.append([])
@@ -59,7 +127,13 @@ def deal_hands(deck, numhands):
 
 
 def hand_score(hand: list[UnoCard]):
+    """Returns the total score of the cards in a hand
+    """
     totalScore = 0
     for card in hand:
         totalScore += card.score_value()
     return totalScore
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
