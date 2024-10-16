1b385706ed3a3d6bd810b308b2647afe62dcad2b
Bluemaster05 <logang@eatonair.us>
2024-10-16 06:40:22 -0400

    lock is done!!!

---
+++ 456 - Adv Web Dev/Class/10-15-24/main.js
@@ -0,0 +1,23 @@
+new Promise((resolve) => {
+    resolve("yes!")
+})
+
+color space
+color gammit
+color powerpoint
+
+rgb and hex conversion
+
+function defintion, XPathExpression, arrow 
+
+const myfunct() => {}
+
+function myFunc() {} FUNCTION HOISTED!!!!
+
+const myfunct = function() {}
+
+READ THE TEXT FOR CASES
+
+GO OVER FIRST 3 POWERPOINTS AND REVIEW SHEET
+
+FILTER MAP SORTING ETC ON TEST
--- 456 - Adv Web Dev/Lab/10-9-24/main.js
+++ 456 - Adv Web Dev/Lab/10-9-24/main.js
@@ -1,4 +1,3 @@
-// debugger
 const lock = document.getElementById("Lock")
 const tick = document.querySelector(".tick")
 const numTicks = lock.getAttribute("data-lock-size")
@@ -6,15 +5,8 @@ const widthAndHeight = Number(lock.getAttribute("data-dimensions"))
 const radius = widthAndHeight / 2
 const degrees = 360 / numTicks 
 const h1 = document.getElementById("lockNum")
-
 lock.style.width = widthAndHeight + "px"
 lock.style.height = widthAndHeight + "px"
-
-// tick.style.transform = `translateX(${radius - 10}px)`
-// tick.style.transform = `rotate(45deg)`
-
-let mousex = 0
-let mousey = 0
 
 function addWidget() {
     let rotation = 0
@@ -22,8 +14,7 @@ function addWidget() {
         const top = radius - 1 + (radius * Math.sin((rotation) * (Math.PI / 180)))
         const left = radius - 0 + (radius * Math.cos((rotation) * (Math.PI / 180)))
         const numTop = radius - 7 + ((radius - 30) * Math.sin((rotation) * (Math.PI / 180)))
-        const numLeft = radius - 4 + ((radius - 30) * Math.cos((rotation) * (Math.PI / 180)))
-
+        const numLeft = radius - 7 + ((radius - 30) * Math.cos((rotation) * (Math.PI / 180)))
 
         // creates new tick
         let newtick = document.createElement("div")
@@ -33,7 +24,7 @@ function addWidget() {
         newtick.style.transform = `rotate(${rotation + 180}deg)`
         newtick.style.transformOrigin = "center left"
         lock.appendChild(newtick)
-        // debugger
+
         //Creates new Number
         let newNum = document.createElement("div")
         newNum.classList.add("tick-num")
@@ -42,56 +33,70 @@ function addWidget() {
         newNum.style.left = `${numLeft}px`
         newNum.style.transform = `rotate(${rotation}deg)`
         lock.appendChild(newNum)
-
-
         rotation += degrees
     }
 }
 addWidget()
-// lock.style.transform = "rotate(15deg)"
 
 let mousedown = false
-let clickX = 0
-let clickY = 0
-lock.addEventListener("mouseenter", () => {
+let referenceRotate = 0
+let currentRotation = 0
+
+const circle = lock.getBoundingClientRect();
+let lockCenterX = circle.x + (circle.width / 2)
+let lockcenterY = circle.y + (circle.height / 2)
+
+lock.addEventListener("mouseover", () => {
     lock.style.cursor = "grab"
     overLock = true
-    console.log("h1")
+})
+
+lock.addEventListener("mousedown", (event) =>{
+    mousedown = true
+    lock.style.cursor = "grabbing"
+    let clickX = event.clientX
+    let clickY = event.clientY
+    referenceRotate = Math.atan2(clickY - lockcenterY, clickX - lockCenterX) * 180 / Math.PI
+
+
 })
 
 
 lock.addEventListener("mousemove", (event) => {
-    let circle = lock.getBoundingClientRect();
-    lockCenterX = circle.x + (circle.width / 2)
-    lockcenterY = circle.y + (circle.height / 2)
-    currentX = event.clientX
-    currentY = event.clientY
-    // xDelta = ( lockCenterX - clickX ) - ( lockCenterX - currentX )
-    // yDelta =  ( lockcenterY - clickY ) - ( lockcenterY - currentY )
-    testy = lockCenterX - currentX 
-    testx = lockcenterY - currentY 
+    
+    let currentX = event.clientX
+    let currentY = event.clientY
+    
     if (mousedown){
+        // Rotate Lock
+        newRotation = Math.atan2(currentY - lockcenterY, currentX - lockCenterX) * 180 / Math.PI
+        currentRotation += (newRotation - referenceRotate)
+        if (currentRotation >= 360){
+            currentRotation %= 360
+        } 
+        else if(currentRotation < 0){
+            currentRotation %= 360
+            currentRotation += 360
+        }
+        console.log(`Diffeerence: ${newRotation - referenceRotate}`)
+        console.log(referenceRotate)
+        referenceRotate = newRotation
+
+        lock.style.transform = `rotate(${(currentRotation)}deg)`
+        h1.style.transform = `rotate(${-currentRotation}deg)`
         
-        lockRotation = (Math.atan2(testy, testx) * 180 / Math.PI)
-        lock.style.transform = `rotate(${(-1 *lockRotation)}deg)`
-        h1.style.transform = `rotate(${lockRotation}deg)`
-        // console.log(lockRotation)
-        // console.log(currentX, currentY)
-        // console.log(xDelta, yDelta)
-        console.log(testx, testy)
+        let h1Num = Math.round(Math.abs(currentRotation - 360) / degrees, 0)
+        if (h1Num == numTicks){
+            h1Num = 0
+        }
+        h1.innerText = `${h1Num}`
     }})
 
 
-lock.addEventListener("mousedown", (event) =>{
-    mousedown = true
-    clickX = event.clientX
-    clickY = event.clientY
-})
 lock.addEventListener("mouseup", (event) => {
     mousedown = false
-    // currentX = event.clientX
-    // currentY = event.clientY
-    // lockRotation = (Math.atan2((currentY-clickY), (currentX-clickX))) * 180 / Math.PI
-    // lock.style.transform = `rotate(${lockRotation}deg)`
-    // console.log(lockRotation)
+    lock.style.cursor = "grab"
+})
+lock.addEventListener("mouseleave", () => {
+    mousedown = false
 })
