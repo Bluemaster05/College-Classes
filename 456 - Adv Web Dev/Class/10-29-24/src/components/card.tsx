89e486ba81b3e8f1d0ce49faae72fa7313438358
Bluemaster05 <logang@eatonair.us>
2024-10-30 13:21:11 -0400

    alotta web

---
+++ 456 - Adv Web Dev/Class/10-29-24/src/components/card.tsx
@@ -0,0 +1,9 @@
+export default function Card(props: { }) {
+    return( <div style={{
+        width: '100px'
+        height: '100px'
+        background: '#ccc'
+    }}></div>)
+}
+
+// export type abc = 'abc'
