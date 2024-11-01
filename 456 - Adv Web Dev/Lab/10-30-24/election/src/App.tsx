acd949b1ecaadba110b421379d3c61b7c96a0a62
Bluemaster05 <logang@eatonair.us>
2024-11-01 10:39:02 -0400

    web dev progress

--- 456 - Adv Web Dev/Lab/10-30-24/election/src/App.tsx
+++ 456 - Adv Web Dev/Lab/10-30-24/election/src/App.tsx
@@ -1,13 +1,25 @@
 import "./App.css"
 import { CandidateCard } from "./components/CandidateCard"
 import { Header } from "./components/Header"
+import { candidates } from "./data/mockData"
 
 function App() {
-     return <>
-    <Header></Header>
-    <section>
-        <CandidateCard></CandidateCard>
-    </section>
+    const all_candidates = []
+    for (let candid of candidates){
+        all_candidates.push(<CandidateCard CanName={candid.name} party={candid.party} votes={5} photo={candid.pictureUrl}></CandidateCard>)
+    }
+    return <>
+        <Header></Header>
+        <section
+        style={{
+            display:'flex',
+            gap: '5px',
+            padding: '10px',
+            justifyContent: 'center',
+            flexWrap: 'wrap'
+        }}>
+            {all_candidates}
+        </section>
     </>
 }
 
--- 456 - Adv Web Dev/Lab/10-30-24/election/src/components/CandidateCard.tsx
+++ 456 - Adv Web Dev/Lab/10-30-24/election/src/components/CandidateCard.tsx
@@ -1,27 +1,52 @@
-export function CandidateCard(){
+export function CandidateCard(props: {CanName: string, party: string, votes: number, photo: string}){
     return <>
     <div style={{
         backgroundColor: "#3e3e3e",
         width: '200px',
-        height: '250px',
-        borderRadius: '10px'
+        height: '260px',
+        borderRadius: '10px',
+        display: "flex",
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: '5px'
     }}>
         <div
         style={{
             width: '190px',
+            height: '25%', 
             backgroundColor: '#2d2d2d',
             display: 'flex',
             flexDirection: 'column',
-            alignItems: 'center'
+            alignItems: 'center',
+            borderRadius: '7px'
         }}>
-            <h1
+            <h1>John Doe</h1>
+            <p
             style={{
-
-            }}>John Doe</h1>
-            <p>Independent</p>
+                padding: '0',
+                margin: '0',
+            }}>Independent</p>
         </div>
-        <div>
-            <img src="" alt="" />
+        <div style={{
+            backgroundColor: 'Green',
+            height: '65%',
+            width: '95%',
+            borderRadius: '10px'
+
+        }}>
+            <div style={{
+                backgroundColor: 'red',
+                border: '2px solid white',
+                width: '35px',
+                height: '35px',
+                borderRadius: '100%',
+                marginTop: '120px',
+                marginLeft: '140px',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center'
+            }}>100</div>
         </div>
     </div>
     </>
