0b74ae29907025d8fbe797d7ecb5e436bdb35c6a
Bluemaster05 <logang@eatonair.us>
2024-12-05 08:38:01 -0500

    fwefwerfg

--- 215-CPTR/Testing/idk/app.py
+++ 215-CPTR/Testing/idk/app.py
@@ -1,28 +1,97 @@
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QLabel
+# Read integers into a list and return the list.
+comparisons = 0
+
+def read_nums():
+    nums = input().split()
+    return [int(num) for num in nums]
+
+# Output the content of a list, separated by spaces.
+def print_nums(numbers):
+    for num in numbers:
+        print (num, end=' ')
+    print()
+
+def merge(numbers, low, pivot, high):
+    global comparisons
+    merged_size = high - low + 1               
+    merged_numbers = []                   
+    for l in range(merged_size):
+        merged_numbers.append(0)
+
+    merge_pos = 0                         
+
+    left_pos = low                          
+    right_pos = pivot + 1                     
+
+    while left_pos <= pivot and right_pos <= high:
+        # comparisons +=1
+        if numbers[left_pos] < numbers[right_pos]:
+            comparisons +=1
+            merged_numbers[merge_pos] = numbers[left_pos]
+            left_pos = left_pos + 1
+        else:
+            comparisons +=1
+            merged_numbers[merge_pos] = numbers[right_pos]
+            right_pos = right_pos + 1
+        merge_pos = merge_pos + 1
+
+    while left_pos <= pivot:
+        # comparisons +=1
+        merged_numbers[merge_pos] = numbers[left_pos]
+        left_pos = left_pos + 1
+        merge_pos = merge_pos + 1
+
+    while right_pos <= high:
+        # comparisons +=1
+        merged_numbers[merge_pos] = numbers[right_pos]
+        right_pos = right_pos + 1
+        merge_pos = merge_pos + 1
+
+    merge_pos = 0
+    while merge_pos < merged_size:
+        # comparisons +=1
+        numbers[low + merge_pos] = merged_numbers[merge_pos]
+        merge_pos = merge_pos + 1
 
 
-class MyWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Simple Gui")
-        self.count = 0
-        self.button = QPushButton(str(self.count))
-        self.button.clicked.connect(self.doStuff)
-        # self.setCentralWidget(self.button)
-        # label = QLabel.
+def merge_sort(numbers, low, high):
+    """
+    >>> merge_sort([1, 4, 2, 5, 6], 2, 4)
+    2 3 | 4 4
+    2 2 | 3 3
+    >>> merge_sort([1, 4, 2, 5, 6], 1, 4)
+    1 2 | 3 4
+    1 1 | 2 2
+    3 3 | 4 4
+    >>> merge_sort([1, 4, 2, 5, 6], 3, 1)
+    
+    """
+    global comparisons
+    pivot = 0
+    # comparisons +=1
+    if low < high:
+        pivot = (low + high) // 2 
+        # Trace output added to code in book
+        print(f'{ low } { pivot } | { pivot + 1 } { high }')
 
-    def doStuff(self):
-        self.count += 1
-        self.button.setText(str(self.count))
+        merge_sort(numbers, low, pivot)
+        merge_sort(numbers, pivot + 1, high)
 
-    def mouseReleaseEvent(self, event):
-        print("realsesd")
+        merge(numbers, low, pivot, high)
 
-    def keyPressEvent(self, event):
-        print(event)
-        
-app = QApplication()
-window = MyWindow()
-window.show()
-app.exec()
+
+if __name__ == '__main__':
+    numbers = read_nums()
+
+    print ('unsorted:', end=' ')
+    print_nums(numbers)
+    print()
+
+    merge_sort(numbers, 0, len(numbers) - 1)
+
+    print ('\nsorted:', end=' ')
+    print_nums(numbers)
+    print(f'comparisons: {comparisons}')
+
+    import doctest
+    doctest.testmod()
